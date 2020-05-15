26c416acd2c50aa481967740746a9f116b94730e
Luke Parker <luke.parker9@tafensw.edu.au>
2020-05-15 15:39:02 +1000

    Created ConsoleLoopDemo

--- ConsoleLoopPattern.js
+++ ConsoleLoopPattern.js
@@ -18,14 +18,14 @@ function askQuestion(question) {
 async function Program() {
     // Your Code Here...
 
-    while(true) {
+    while (true) {
         console.log("Welcome to Luke's Calculator");
         console.log("[1] Add numbers");
         console.log("[2] Subtract numbers");
         console.log("[3] Multiply numbers");
         console.log("[4] Exit");
         let ans = await askQuestion("Enter a number to select an option from the list above");
-        if(ans == "1") {
+        if (ans == "1") {
             console.log();
             console.log("You have selected the Add numbers function");
             let input1 = await askQuestion("Enter the first number: ");
@@ -43,7 +43,7 @@ async function Program() {
             let number2 = parseInt(input2);
             console.log(`The difference between ${number1} and ${number2} is ${number1 - number2}`);
             console.log();
-        } else if (ans == "2") {
+        } else if (ans == "3") {
             console.log();
             console.log("You have selected the Multiply numbers function");
             let input1 = await askQuestion("Enter the first number: ");
